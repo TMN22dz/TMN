a6c41d0525d194ba7b819f522b4d81abfbeb321a
TMN22dz <122946636+TMN22dz@users.noreply.github.com>
2023-01-29 23:37:25 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const menuHamburger = document.querySelector(".menu");
+const navbarre = document.querySelector(".nav-barre nav")
+const navBarre = document.querySelector(".nav-barre ")
+menuHamburger.addEventListener('click',()=>{
+navbarre.classList.toggle('mobilmenu')
+navBarre.classList.toggle('mobilmenu')
+})
+
