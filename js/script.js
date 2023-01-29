b9ff01a87a8c9ecd912f35a029b4dd1087c10da0
TMN22dz <122946636+TMN22dz@users.noreply.github.com>
2023-01-29 23:10:35 +0100

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const menuHamburger = document.querySelector(".menu");
-const navbarre = document.querySelector(".nav-barre nav")
-menuHamburger.addEventListener('click',()=>{
-navbarre.classList.toggle('mobilmenu')
-})
-
